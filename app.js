c9f9cb8e9127b5921827e09248e23a6ecac711d2
Anthony <anthony.kizer88@gmail.com>
2019-08-01 12:06:33 -0700

    finished about page, added github links of all team

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
+//modal
 const btn = document.getElementsByClassName("signup-btn");
-
 const modal = document.querySelector(".modalform");
-
 const body = document.querySelector(".notmodal");
-
 const closeBtn = document.querySelector(".close-window");
 
+//names on about
+const anthony = document.querySelector(".anthony");
+const jean = document.querySelector(".jean");
+const alex = document.querySelector(".alex");
+const miguel = document.querySelector(".miguel");
+const brian = document.querySelector(".brian");
+const zach = document.querySelector(".zach");
+const manny = document.querySelector(".manny");
+const eric = document.querySelector(".eric");
+
+//sign up buttons, top and bottom of page
 btn[0].addEventListener("click", showModal);
 btn[1].addEventListener("click", showModal);
 
+//modal event listeners
 closeBtn.addEventListener("click", closeWindow);
-
 window.addEventListener("click", closeModal);
 
+//modal functions
 function showModal() {
   modal.style.display = "block";
   body.style.filter = "blur(10px)";
@@ -28,3 +38,7 @@ function closeModal(e) {
     body.style.filter = "blur(0)";
   }
 }
+
+//about event listeners
+
+//about functions
